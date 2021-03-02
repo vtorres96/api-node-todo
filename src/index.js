7d90d896c116cceda314dc780422dc8d27de6d9d
vtorres96 <victorcdc96@gmail.com>
2021-03-02 18:45:56 -0300

    configurando porta

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('Servidor rodando na porta http://localhost:3333')
-});
+app.listen(process.env.PORT || 3333);
 
 module.exports = app;
